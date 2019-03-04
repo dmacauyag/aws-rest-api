abfdbc3efb329f60bd12dd025d62cc20689185ce
Daniel Macauyag <dmacauyag92@gmail.com>
2019-03-03 16:29:03 -0800

    add files for seeding users table with mock data locally

---
+++ seed/runner.js
@@ -0,0 +1,39 @@
+require('dotenv/config')
+
+const { UserSeeder } = require('./user.seeder')
+const { DynamoDB } = require('aws-sdk')
+const { DocumentClient } = DynamoDB
+const usersMockData = require('./users-test-data.json')
+
+const dynamo = new DynamoDB({
+  endpoint: process.env.AWS_ENDPOINT,
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const doclient = new DocumentClient({ service: dynamo })
+const userSeeder = new UserSeeder(dynamo, doclient)
+
+const log = (...mgs) => console.log('>>', ...mgs)
+
+const seedUsers = async () => {
+  log(`Checking if 'users' table exists`)
+
+  const exists = await userSeeder.hasTable()
+
+  if (exists) {
+    log(`Table 'users' exists, deleting`)
+    await userSeeder.deleteTable()
+  }
+
+  log(`Creating 'users' table`)
+  await userSeeder.createTable()
+
+  log('Seeding data')
+  await userSeeder.seed(usersMockData)
+}
+
+seedUsers()
+  .then(() => log('Done!'))
+  .catch(err => console.log(err))
