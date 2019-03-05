ce2d0ef8c42b25c66b5ab34535b790cea8f5c57c
Daniel Macauyag <dmacauyag92@gmail.com>
2019-03-04 22:29:57 -0800

    JSON.stringify lambda response

--- src/handlers/users/add.js
+++ src/handlers/users/add.js
@@ -5,7 +5,7 @@ const { parseWith } = require('../../utils/request.util')
 const { createRepository } = require('../../utils/repository.util')
 
 const repository = createRepository(process.env)()
-const created = withStatusCode(201)
+const created = withStatusCode(201, JSON.stringify)
 const parseJson = parseWith(JSON.parse)
 
 exports.handler = async (event) => {
